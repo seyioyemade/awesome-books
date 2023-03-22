aa9796a69e03dac55ca1162687923a5ba12905d5
Seyi Oyemade <seyioyemade918@gmail.com>
2023-03-22 13:37:07 +0100

    Get data from local storage and remove book from list

--- script.js
+++ script.js
@@ -1,88 +1,68 @@
-let books = [
-
-]
+let books = JSON.parse(localStorage.getItem('BookInformation')) || [];
 
 const section = document.querySelector('.all-books');
 const newTitle = document.querySelector('#title');
 const newAuthor = document.querySelector('#author');
-const addBookBtn = document.querySelector('.add-book>button');
 const form = document.querySelector('form');
 
-const removeBook = function (element, i) {
+function removeBook(id) {
+  books = books.filter((item) => item.id !== Number(id));
 
-  for (let i = 0; i<books.length; i++){
-    books.splice(i, 1)
-  }
-  
+  localStorage.setItem('BookInformation', JSON.stringify(books));
+  const currentBooks = JSON.parse(localStorage.getItem('BookInformation'));
+
+  const allBooks = document.querySelectorAll('.book');
+
+  allBooks.forEach((book) => {
+    book.style.display = 'none';
+  });
+
+  currentBooks.forEach((book) => {
+    displayBooks(book.title, book.author, book.id);
+  });
 }
 
-books.filter(removeBook)
-
-function displayBooks(title, author) {
+function displayBooks(title, author, id) {
   const div = document.createElement('div');
   div.className = 'book';
+  div.id = id;
   const titleParagragh = document.createElement('p');
   titleParagragh.textContent = title;
   const authorParagraph = document.createElement('p');
   authorParagraph.textContent = author;
   const button = document.createElement('button');
   button.textContent = 'Remove';
+
   button.addEventListener('click', () => {
-  
+    removeBook(div.id);
   });
+
   const divider = document.createElement('hr');
   div.append(titleParagragh, authorParagraph, button, divider);
   section.appendChild(div);
 }
 
+books.forEach((book) => {
+  displayBooks(book.title, book.author, book.id);
+});
 
-const addBook = function() {
-
-  displayBooks( newTitle.value, newAuthor.value)
-  // localStorage.clear();
-
+const addBook = function () {
   const book = {
+    id: Math.floor(Math.random() * 10000000),
     title: newTitle.value,
     author: newAuthor.value,
-  }
+  };
 
-  books.push(book)
+  books.push(book);
 
   localStorage.setItem('BookInformation', JSON.stringify(books));
-}
-
+  displayBooks(newTitle.value, newAuthor.value, book.id);
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  // const titleInput = document.getElementById('titleInput');
-  // const authorInput = document.getElementById('authorInput');
-  addBook();
-  // titleInput.value = '';
-  // authorInput.value = '';
+
+  if (!(newTitle.value === '' || newAuthor.value === '')) { addBook(); }
+  newTitle.value = '';
+  newAuthor.value = '';
 });
-  // localStorage.setItem('BookInformation', JSON.stringify(BookInformation));
-
-
-
-const displayStorageBooks = function() {
-
-  if (localStorage.getItem('BookInformation')) {
-    const storageInformation = JSON.parse(localStorage.getItem('BookInformation'));
-    console.log (storageInformation)
-
-    for (let i = 0; i<storageInformation.length; i++){
-      displayBooks( storageInformation[i].title, storageInformation[i].author)
-      
-    }
-
-  }
-
-}
-
-
-document.addEventListener("DOMContentLoaded", displayStorageBooks);
-
-
-
-// console.log (book)
-
